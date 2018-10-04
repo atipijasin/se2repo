896239213bd50e816bef74750590f3a358767ef1
Jasin Atipi <atipiJ@users.noreply.github.com>
2018-10-04 09:32:13 +0200

    meatspin

--- api.js
+++ api.js
@@ -6,10 +6,12 @@ var courses_offered = [{id: 21, name: 'HCI'},
 {id: 28, name:'sweng'}]
  
  
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.redirect('https://trentose2.herokuapp.com'))
  
 app.get('/courses', (req, res) => {
    res.json(courses_offered)
 })
- 
+
+
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
