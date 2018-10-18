5ad04009d9cba970adcbf100d99416ee15077976
Jasin Atipi <atipiJ@users.noreply.github.com>
2018-10-18 08:58:09 +0200

    removed meatspin

--- api.js
+++ api.js
@@ -6,8 +6,8 @@ var courses_offered = [{id: 21, name: 'HCI'},
 {id: 28, name:'sweng'}]
  
  
-// app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.redirect('https://trentose2.herokuapp.com'))
+app.get('/', (req, res) => res.send('<h1>It works!</h1><br>This is the default web page for this server. <br> The web server software is running but no content has been added, yet.'))
+// app.get('/', (req, res) => res.redirect('https://trentose2.herokuapp.com'))
  
 app.get('/courses', (req, res) => {
    res.json(courses_offered)
--- client.js
+++ client.js
@@ -1,12 +1,19 @@
 const fetch = require("node-fetch");
-const url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+const url = "https://se2trento.herokuapp.com/courses"
  
 const getLocation = async url => {
  
   try {
     const response = await fetch(url);
     const json = await response.json();
-    console.log(json)
+    app.get('/', (req, res) => {
+      res.send("-- Received following courses --")
+      json.forEach(function(course) {
+          res.send("Course id: " + course.id);
+          res.send("Course name: " + course.name );
+          res.send("------------------------------" );
+      }, this);
+    }
   } catch (error) {
     console.log(error);
   }
