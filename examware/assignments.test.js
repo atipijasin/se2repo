376279ebd9f5f276d30c4c30edfb4687089510dc
Jasin Atipi <jasin.atipi@gmail.com>
2018-12-17 16:21:49 +0100

    fixed test

--- examware/assignments.test.js
+++ examware/assignments.test.js
@@ -15,15 +15,3 @@ test('putAssignment module should be defined', () => {
 test('deleteAssignment module should be defined', () => {
     expect(assignments.deleteAssignment).toBeDefined();
 });
-
-test('posting with valid data should return the passed data', () =>{
-    let body = {
-        taskId: 'pitagora',
-        assignmentId: 'giugno',
-        workerId: 'giacomo',
-        assignmentResult: {}
-    };
-    return assignments.postAssignment(body).then(resBody => {
-        expect(resBody).toBe(body);
-    });
-})
